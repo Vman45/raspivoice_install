c4d52c8c080a1c8a3f8efc5c1b4a849c2c0a094d
raspivoice <support@raspivoice.com>
2015-11-08 01:20:21 +1300

    Updated install instructions

--- install_raspivoice.sh
+++ install_raspivoice.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-if [ "$(whoami)" != "root" ]; then
+if [ "$(whoami)" == "root" ]; then
 
-    echo "Sorry, you must be run this command as root."
-    echo "For example : 'sudo install_raspivoice.sh'"
+    echo "Sorry, you should not run this command as root."
+    echo "For example  NOT not used 'sudo' when you call this script"
     exit 1
 fi
 
 cd $HOME/bin
-TAR_FILENAME=raspivoice.tar
+TAR_FILENAME=raspivoice_pi.tar
 
 sudo apt-get install git
 
@@ -15,7 +15,7 @@ sudo apt-get install git
 substring="Ubuntu"
 string1= lsb_release -a | grep $substring
 
-if test "${string1#*$substring}" != $substring
+if test "${string1#*$substring}" == $substring
 then
 	echo "Running on Ubuntu"
 	TAR_FILENAME=raspivoice_ubuntu.tar											
@@ -23,37 +23,15 @@ else
 	echo "Running on Raspberry Pi"
 fi
 
-#  Get the binary excutable for the raspivoice application itself
-wget http://www.grannymon.com/raspivoice/${TAR_FILENAME}
-if  test -s $TAR_FILENAME  
-then
-	echo "Successully downloaded $tarfilename"
-	tar xvf $TAR_FILENAME
-	rm $TAR_FILENAME
-else
-	echo "$TAR_FILENAME not found"
-fi
 
-# Install eSpeak
-apt-get install espeak
-
-echo
-file="/usr/local/raspi-wav/.git/index"
-if  test -s "$file" 
+if  test -s "$TAR_FILENAME" 
 then 
-    cd "/usr/local/raspi-wav/"
-    sudo git fetch
-else 
-    cd /usr/local
-    pwd
-    echo
-	echo
-	echo NOTE : The next step is download all the WAV files
-	echo        This can take a long time as there are lots of them.
-	echo        I suggest you go get a cup of tea and come back in half an hour
-	read -p "Press any key to continue... " -n1 -s
-    sudo git clone https://github.com/raspivoice/raspi-wav.git
+    echo "Exctracting binaries from $TAR_FILENAME into $HOME/bin"
+	tar xvf $TAR_FILENAME -C $HOME/bin
+else
+  echo "ERROR :  File not found : $TAR_FILENAME"
 fi
+
 
 
 
